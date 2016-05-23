a7ace5a2fa048a6d96891654aca39fa29ac36836
wuuhren <wuduhren@outlook.com>
2016-05-23 11:02:00 +0800

    initial commit

---
+++ Mr-Ride-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mr-Ride-iOS
+//
+//  Created by Eph on 2016/5/23.
+//  Copyright © 2016年 AppWorks School WuDuhRen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
