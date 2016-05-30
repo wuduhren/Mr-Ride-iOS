254279e07f02e4dfebd91796eadc3a5811967d19
wuuhren <wuduhren@outlook.com>
2016-05-30 09:14:24 +0800

    5/30 9:14

---
+++ Mr-Ride-iOS/StopWatch.swift
@@ -0,0 +1,48 @@
+//
+//  StopWatch.swift
+//  Mr-Ride-iOS
+//
+//  Created by Eph on 2016/5/29.
+//  Copyright © 2016年 AppWorks School WuDuhRen. All rights reserved.
+//
+
+import Foundation
+
+class Stopwatch {
+    init() { }
+    
+    private var startTime: NSDate?
+    
+    var intervalBeforPause: NSTimeInterval?
+    
+    var elapsedTimeLabelText: String = ""
+    
+    var elapsedTime: NSTimeInterval {
+        if let startTime = self.startTime {
+            return -startTime.timeIntervalSinceNow
+        } else {
+            return 0
+        }
+    }
+    
+    var isRunning: Bool {
+        return startTime != nil
+    }
+    
+    func start() {
+        startTime = NSDate()
+    }
+    
+    func stop() {
+        elapsedTimeLabelText = "00:00:00.00"
+        intervalBeforPause = nil
+        startTime = nil
+    }
+    
+    func pause() {
+        intervalBeforPause = elapsedTime
+        startTime = nil
+        
+    }
+    
+}
