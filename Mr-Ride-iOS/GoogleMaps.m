a23c4b3ebcf2783644afe04589841e965be793ae
wuuhren <wuduhren@outlook.com>
2016-06-22 17:31:08 +0800

    file rearranged

--- Mr-Ride-iOS/GoogleMaps.m
+++
@@ -1,9 +0,0 @@
-//
-//  GoogleMaps.m
-//  Mr-Ride-iOS
-//
-//  Created by Eph on 2016/5/30.
-//  Copyright © 2016年 AppWorks School WuDuhRen. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
